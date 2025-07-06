e51f2f48d7444d67b6497f57779d3a8ab1d544a8
kyilmaz <kyilmaz@users.noreply.github.com>
2025-07-07 01:23:07 +0300

    Enhance shell experience with new aliases and a custom prompt

--- popos-ws-setup.sh
+++ popos-ws-setup.sh
@@ -273,10 +273,54 @@ alias py='python3'
 alias dc='docker compose'
 alias dps='docker ps'
 
-# Add paths
-export PATH="$HOME/.cargo/bin:$HOME/anaconda3/bin:$PATH"
+# Interactive operation...
+alias rm='rm -i'
+alias cp='cp -i'
+alias mv='mv -i'
+alias ln='ln -i'
+alias c='clear'
+
+# Default to human readable figures
+alias df='df -h'
+alias du='du -h'
+
+# Misc
+alias less='less -r'                          # raw control characters
+alias whence='type -a'                        # where, of a sort
+alias grep='grep --color'                     # show differences in colour
+alias egrep='egrep --color=auto'              # show differences in colour
+alias fgrep='fgrep --color=auto'              # show differences in colour
+
+# Some shortcuts for different directory listings
+alias ls='ls -hF --color=tty'                 # classify files in colour
+alias dir='ls --color=auto --format=vertical'
+alias vdir='ls --color=auto --format=long'
+
 EOF
 fi
+
+cp ~/.profile ~/.profile.backup.$(date +%Y%m%d)
+cat << 'EOF' >> ~/.profile
+
+
+export TMOUT=
+
+export LC_ALL=C
+
+export LANG=en_US.UTF-8
+
+# Function remotedisplay get remote XDISPLAY running
+function remotedisplay() {
+  remoteip=$(who am i | awk '{print $NF}' | tr -d ')''(' )
+}
+remotedisplay
+DISPLAY=$remoteip:0.0; export DISPLAY
+
+export PS1="\033[38;5;209m\]┌──[\033[38;5;141m\]\u\033[38;5;209m\]:\033[38;5;105m\]\h\033[38;5;231m\]\W\033[38;5;209m\]]\n\033[38;5;209m\]└─\\[\033[38;5;209m\]$\[\033[37m\] "
+
+export PATH="$HOME/.cargo/bin:$HOME/anaconda3/bin:$PATH"
+
+EOF
 
 log "Installing Flatpak support..."
 sudo apt-get install -y flatpak gnome-software-plugin-flatpak
